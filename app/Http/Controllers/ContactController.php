152ffb1b56584cf398a1bb620b5044c5de6d1d6c
sanafyz <fyz.sana@yahoo.com>
2018-03-30 19:32:21 +0500

    contact us page done

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Contact;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+         $contacts = Contact::all();
+
+        // load the view and pass the nerds
+       return view('contacts.index', array('contacts' => $contacts));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+         return view('contacts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // validate
+        // read more on validation at http://laravel.com/docs/validation
+         $validatedData = $request->validate([
+        'name' => 'required',
+        'comment' => 'required',
+    ]);
+
+        // process the login
+      
+
+   
+
+            $contact = new Contact;
+            $contact->name       = $request->get('name');
+            $contact->email      = $request->get('email');
+            $contact->phone = $request->get('phone');
+            $contact->comment = $request->get('comment');
+            $contact->save();
+
+            // redirect
+            
+            return redirect()->route('contacts.index')->with('message', 'I am so frustrated.');
+       
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
